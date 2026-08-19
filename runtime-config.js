f32b39bee454e8f52ce36ebfbccd3c04ac854a2a
aashyacosmetics <syoat.amazon@gmail.com>
2026-08-19 15:25:13 +0530

    Add files via upload

---
+++ runtime-config.js
@@ -0,0 +1,5 @@
+// Local deployment configuration. This file is gitignored.
+window.SYOAT_CONFIG = Object.freeze({
+  environment: "production",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwqBI7uPnv63VYms4PHaJceFhNzEf8Y6f-6ni-2LT2G_1pWmL7WpdwK4uu6HeAuW67agg/exec"
+});
